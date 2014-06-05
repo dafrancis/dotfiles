8655bb2c5554ce41537e96c317ab7f753309065b
Dafydd Francis <dafydd.francis@andwhathave.eu>
2014-06-05 01:22:23 +0100

    remove old packages automatically

--- dot/subcommands/update.sh
+++ dot/subcommands/update.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+function remove_old_packages() {
+    cd $HOME/.dotfiles/vim/bundle
+    git status -s | grep -E "^\?\?" | cut -d ' ' -f 2 | xargs rm -rf
+}
+
 function main() {
     echo "Updating dotfiles..."
     cd $HOME/.dotfiles
     git pull
     git submodule update --init
     git submodule foreach git pull origin master
+    remove_old_packages
     git commit "$HOME/.vim/bundle/" -m "Updating vim submodules"
 }
 
