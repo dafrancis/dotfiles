85b51f7961b616291b2fb34d0b8d35b9a87cadcc
Dafydd Francis <dafydd.francis@andwhathave.eu>
2014-07-07 16:16:40 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -10,3 +10,11 @@ ln -fs ~/.dotfiles/zshrc ~/.zshrc
 # Link vim
 ln -fs ~/.dotfiles/vimrc ~/.vimrc
 ln -fs ~/.dotfiles/vim ~/.vim
+
+# Would you believe that neovim is literally the same as vim?
+# yes. yes you would.
+ln -fs ~/.dotfiles/vimrc ~/.nvimrc
+ln -fs ~/.dotfiles/vim ~/.nvim
+
+# So how about them window managers
+ln -fs ~/.dotfiles/hydra ~/.hydra
