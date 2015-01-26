761bf6ea38c6d298b56cb7e40a29f86983e7b243
Dafydd Francis <dafydd.francis@fundtech.com>
2015-01-26 11:58:37 +0000

    Flesh out setup.sh but not really

--- setup.sh
+++ setup.sh
@@ -1,20 +1,59 @@
-# Setup zsh as default shell
-chsh -s /bin/zsh
+#!/bin/bash
 
-# Install Oh My Zsh
-curl -L http://install.ohmyz.sh | sh
+set -e
 
-# Link zshrc
-ln -fs ~/.dotfiles/zshrc ~/.zshrc
+function is_mac() {
+    [ "`uname`" == "Darwin" ]
+}
 
-# Link vim
-ln -fs ~/.dotfiles/vimrc ~/.vimrc
-ln -fs ~/.dotfiles/vim ~/.vim
+function setup_zsh() {
+    # Setup zsh as default shell
+    chsh -s /bin/zsh
 
-# Would you believe that neovim is literally the same as vim?
-# yes. yes you would.
-ln -fs ~/.dotfiles/vimrc ~/.nvimrc
-ln -fs ~/.dotfiles/vim ~/.nvim
+    # Install Oh My Zsh
+    curl -L http://install.ohmyz.sh | sh
 
-# So how about them window managers
-ln -fs ~/.dotfiles/hydra ~/.hydra
+    # Link zshrc
+    ln -fs ~/.dotfiles/zshrc ~/.zshrc
+}
+
+function setup_bash() {
+}
+
+function setup_shell() {
+    if hash zsh 2>/dev/null; then
+        echo "-- zsh detected, using zsh..."
+        setup_zsh
+    else
+        echo "-- zsh not detected, using bash..."
+        setup_bash
+    fi
+}
+
+function setup_vim() {
+    # Link vim
+    ln -fs ~/.dotfiles/vimrc ~/.vimrc
+    rm -rf ~/.vim
+    ln -fs ~/.dotfiles/vim ~/.vim
+
+    # Would you believe that neovim is literally the same as vim?
+    # yes. yes you would.
+    ln -fs ~/.dotfiles/vimrc ~/.nvimrc
+    ln -fs ~/.dotfiles/vim ~/.nvim
+}
+
+function setup_hydra() {
+    # So how about them window managers
+    ln -fs ~/.dotfiles/hydra ~/.hydra
+}
+
+function main() {
+    setup_shell
+    setup_vim
+    if `is_mac`; then
+        echo "-- Mac detected, installing mac specific stuff..."
+        setup_hydra
+    fi
+}
+
+main
